be4d5e02344fedf3025f25fcdce16726dbd2c419
usb777 <dzsamoila@gmail.com>
2021-03-09 18:49:48 -0500

    rev and sol added

---
+++ Rev_Pack/PetService/src/main/java/com/revature/controller/PetController.java
@@ -0,0 +1,43 @@
+package com.revature.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.Pet;
+import com.revature.remote.UserClient;
+
+@RestController
+@RequestMapping("pets")
+public class PetController {
+	
+	private UserClient uc;
+	
+	@Autowired
+	public PetController(UserClient uc) {
+		this.uc = uc;
+	}
+	
+	
+	@GetMapping
+	public List<Pet> getAll() {
+		Pet cat = new Pet("Mittens", "Cat", 1, null);
+		cat.setOwner(uc.getUserById(cat.getOwnerId()));
+		
+		Pet cat1 = new Pet("Mittens1", "Cat1", 1, null);
+		cat.setOwner(uc.getUserById(cat.getOwnerId()));
+		
+		List<Pet> allPets = new ArrayList<Pet>();
+		
+		
+		allPets.add(cat);
+		allPets.add(cat1);
+		return allPets;
+	}
+	
+	
+}
